fecb2fa4322bc0ba34ea988db7447352904ef7a6
Naratip <naratip.18257@gmail.com>
2023-07-13 19:05:26 +0700

    add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <div class="general">
+            <h2>General Information</h2>
+            <div className="form-row">
+              <label htmlFor="name">Name:</label>
+              <input id="name" type="text"></input>
+            </div>
+            <div className="form-row">
+              <label htmlFor="email">Email:</label>
+              <input id="email" type="email"></input>
+            </div>
+            <div className="form-row">
+              <label htmlFor="phone">Phone:</label>
+              <input id="phone" type="tel"></input>
+            </div>
+          </div>
+          <div class="study-info">
+            <h2>Education Experience</h2>
+            <div className="form-row">
+              <label htmlFor="school">School Name:</label>
+              <input id="school" type="text"></input>
+            </div>
+            <div className="form-row">
+              <label htmlFor="study">Title of study:</label>
+              <input id="study" type="text"></input>
+            </div>
+            <div className="form-row">
+              <label htmlFor="study-date">Date of study:</label>
+              <input id="study-date" type="date"></input>
+            </div>
+          </div>
+          <div class="prac-ex">
+            <h2>Practical Experience</h2>
+            <div className="form-row">
+              <label htmlFor="company">Company Name:</label>
+              <input id="company" type="text"></input>
+            </div>
+            <div className="form-row">
+              <label htmlFor="position">Position:</label>
+              <input id="position" type="text"></input>
+            </div>
+            <div className="form-row">
+              <label htmlFor="task">Task of job:</label>
+              <textarea id="task" type="text"></textarea>
+            </div>
+            <div className="form-row">
+              <label htmlFor="work-ex">Working Experience(Years):</label>
+              <input id="work-ex" type="number" min="1" value={1}></input>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
